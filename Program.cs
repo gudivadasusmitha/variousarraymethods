8ec07f9882784a57913af1cbac90f32599f05c51
gudivadasusmitha <Gudivada.Susmitha@ibm.com>
2022-03-02 14:53:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variousarraymethods
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int[] arr = new int[] { 1, 9, 7, 5, 9 };
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            foreach(int value in arr)
+            {
+                Console.WriteLine(value + "  ");
+            }
+        }
+    }
+}
